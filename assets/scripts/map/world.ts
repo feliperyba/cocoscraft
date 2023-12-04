03232611baa40c9d50298470315ce76e45f433e4
Rybones <feliperybakovas@hotmail.com>
2023-12-04 09:23:44 +0000

    small fixes

--- assets/scripts/map/world.ts
+++ assets/scripts/map/world.ts
@@ -1,5 +1,4 @@
 import { _decorator, CCFloat, Component, instantiate, Prefab, Vec3 } from 'cc';
-import { director } from 'cc';
 
 import { Chunk } from './chunk';
 import { ChunkData } from './chunkData';
@@ -60,7 +59,7 @@ export class World extends Component {
             chunkRenderer.updateChunkWithData(meshData);
 
             // this is for later when using dynamic meshes
-            chunkRenderer.meshRender.onGeometryChanged();
+            // chunkRenderer.meshRender.onGeometryChanged();
             // chunkRenderer.meshRenderWater.onGeometryChanged();
 
             this.node.addChild(chunkObject);
--- assets/utils/phsyxDebug.ts
+++ assets/utils/phsyxDebug.ts
@@ -1,64 +1,32 @@
-import { _decorator, Component, director, Node, ResolutionPolicy, view } from 'cc';
+import { _decorator, Component, director, Node } from 'cc';
+import { ResolutionPolicy, view } from 'cc';
 
-import { World } from '../scripts/map/world';
 import { eGroup } from './easyMenu/src/eGroup';
 import { eMenu } from './easyMenu/src/eMenu';
-import { GameManager } from './gameManager';
 const { ccclass, property, type } = _decorator;
 
-@ccclass('VoxelDebug')
-export class VoxelDebug extends Component {
+@ccclass('PhysxDebug')
+export class PhysxDebug extends Component {
     @type(eMenu)
     easyMenu!: eMenu;
 
-    @type(World)
-    worldReferece!: World;
-
     @property(Node)
     postReference!: Node;
-
-    @type(GameManager)
-    gameManager!: GameManager;
 
     group: eGroup;
 
     onLoad(): void {
         this.group = this.easyMenu.addGroup('Debug');
 
-        this.group.addItem('Reset Voxel Scene', () => {
-            director.loadScene('voxel-map');
-        });
-
-        this.group.addItem('Go to PhysX Scene', () => {
+        this.group.addItem('Reset PhysX Scene', () => {
             director.loadScene('main');
         });
-
-        this.group.addEdit('Map Size', this.worldReferece.mapSizeInChunks, value => {
-            this.worldReferece.mapSizeInChunks = Number(value);
-        });
-
-        this.group.addEdit('Chunk Size', this.worldReferece.chunkSize, value => {
-            this.worldReferece.chunkSize = Number(value);
-        });
-
-        this.group.addEdit('Chunk Height', this.worldReferece.chunkHeight, value => {
-            this.worldReferece.chunkHeight = Number(value);
-        });
-
-        this.group.addEdit('Water Threshold', this.worldReferece.waterThreshold, value => {
-            this.worldReferece.waterThreshold = Number(value);
-        });
-
-        this.group.addEdit('Noise Scale', this.worldReferece.noiseScale, value => {
-            this.worldReferece.noiseScale = Number(value);
+        this.group.addItem('Go to Voxel Scene', () => {
+            director.loadScene('voxel-map');
         });
 
         this.group.addToggle('Post Processing', value => {
             this.postReference.active = value;
-        });
-
-        this.group.addItem('Spawn Player', () => {
-            this.gameManager.spawnPlayer();
         });
 
         this.makeResponsive();
